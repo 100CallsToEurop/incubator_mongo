2e02e066b9fa24c3cc0ee3f1718b059c6a3c1b9b
100CallsToEurop <piaterik123123@mail.ru>
2022-11-11 17:38:57 +0300

    test errors

--- src/modules/blogs/application/dto/blog.dto.ts
+++ src/modules/blogs/application/dto/blog.dto.ts
@@ -2,10 +2,10 @@ import { IsString, IsUrl, MaxLength } from 'class-validator';
 import { BlogInputModel } from '../types/blog-input-model.type';
 
 export class BlogDto implements BlogInputModel {
-  @IsString()
+  //@IsString()
   @MaxLength(15)
   readonly name: string;
-  @IsString()
+  //@IsString()
   @MaxLength(100)
   @IsUrl()
   readonly youtubeUrl: string;
