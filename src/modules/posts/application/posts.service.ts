f1108d3e87e5d0c1d227e78980759c6271589ee9
100CallsToEurop <piaterik123123@mail.ru>
2022-11-11 21:36:59 +0300

    test errors

--- src/modules/posts/application/posts.service.ts
+++ src/modules/posts/application/posts.service.ts
@@ -17,7 +17,7 @@ export class PostsService {
       shortDescription: post.shortDescription,
       content: post.content,
       blogId: post.blogId,
-      //blogName: post.blogName,
+      blogName: post.blogName,
     };
   }
 
