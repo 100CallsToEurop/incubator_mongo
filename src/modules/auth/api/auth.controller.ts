9ff84d5b7dbe45bae221516fd6e976785e149848
100CallsToEurop <piaterik123123@mail.ru>
2022-12-03 22:45:54 +0300

    pls

--- src/modules/auth/api/auth.controller.ts
+++ src/modules/auth/api/auth.controller.ts
@@ -54,7 +54,8 @@ export class AuthController {
     res.cookie('refreshToken', tokens.refreshToken, {
       maxAge: 4000 * 1000,
       httpOnly: true,
-      //secure: true,
+      secure: true,
+
     });
     return {
       accessToken: tokens.accessToken,
