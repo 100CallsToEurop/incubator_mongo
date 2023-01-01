3febcef52f30e152e8428914c2eb5a4b89075c2a
100CallsToEurop <piaterik123123@mail.ru>
2023-01-01 13:37:48 +0300

    zopa2

--- src/modules/users/api/models/ban.user.model.ts
+++ src/modules/users/api/models/ban.user.model.ts
@@ -1,9 +1,9 @@
-import { IsBoolean, IsString, MaxLength } from "class-validator"
+import { IsBoolean, IsString, MinLength } from "class-validator"
 
 export class BanUserInputModel{
     @IsBoolean()
     readonly isBanned: boolean
     @IsString()
-    @MaxLength(20)
+    @MinLength(20)
     readonly banReason: string
 }
