21005d1cf766c97581e391b08b8dc161ae4a16e4
100CallsToEurop <piaterik123123@mail.ru>
2023-01-02 11:06:40 +0300

    test zopa

--- src/modules/posts/application/useCases/create-post.use-case.ts
+++ src/modules/posts/application/useCases/create-post.use-case.ts
@@ -1,3 +1,4 @@
+import { ForbiddenException } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { InjectModel } from '@nestjs/mongoose';
 import { PostInputModel } from '../../api/models';
@@ -21,6 +22,9 @@ export class CreatePostUseCase implements ICommandHandler<CreatePostCommand> {
     const { post, userId } = command;
     const blog = await this.postsRepository.getGetBlog(post.blogId)
     const blogName = blog.getName()
+    if (blog.checkOwnerBlog(userId)) {
+      throw new ForbiddenException();
+    }
     const newPostEntity = new PostEntity(post, blogName, userId);
     const newPost = this.PostModel.createPost(newPostEntity, this.PostModel);
     await this.postsRepository.save(newPost);
