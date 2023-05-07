b8b746dd78408b6a37a9325b6dc12eec374a9c80
100CallsToEurop <piaterik123123@mail.ru>
2023-05-07 13:07:23 +0300

    fix

--- src/modules/blogs/application/useCases/upload.post.images.ts
+++ src/modules/blogs/application/useCases/upload.post.images.ts
@@ -66,8 +66,6 @@ export class UploadPostImagesUseCase
       .toBuffer();
     imagesBuffersToUpload.push({ size: 'SMALL', buffer: smallBuffer });
 
-    post.images.main = [];
-
     for (const image of imagesBuffersToUpload) {
       const url = await this.minioClientService.uploadPostMainImage(
         postId,
